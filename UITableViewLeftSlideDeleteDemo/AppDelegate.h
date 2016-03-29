a2389d6862850bba3f88860121ccb321d25a2339
UILabel <shoupeng.ji@urgoo.cn>
2016-03-29 16:49:24 +0800

    原生侧滑多选按钮
    原生侧滑多选按钮(置顶/删除)

---
+++ UITableViewLeftSlideDeleteDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewLeftSlideDeleteDemo
+//
+//  Created by admin on 16/3/29.
+//  Copyright © 2016年 AlezJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewLeftSlideDeleteDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewLeftSlideDeleteDemo
+//
+//  Created by admin on 16/3/29.
+//  Copyright © 2016年 AlezJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
